82f872a5af46e5c3f946347ee7abcca23d78ab37
Dbyu85 <Dbyu85@users.noreply.github.com>
2020-04-28 13:31:09 +0200

    Adding textbox, javascript

--- run.py
+++ run.py
@@ -1,21 +1,17 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
 app.secret_key = "randomnumber123"
 messages = []
 
-def add_messages(username, message):
+def add_message(username, message):
 	""" Add messages to the 'messages' list """
 	#in line 11 is a method to for getting time from python.
 	now = datetime.now().strftime("%H:%M:%S")
-	messages.append("({}) {}: {}".format(now, username, message))
 	
-	
-def get_all_messages():
-	""" get all of the messages and separate them witha 'br' """
-	return "<br>".join(messages)
+	messages.append({"timestamp": now, "from": username, "message": message})
 
 @app.route('/', methods =["GET", "POST"])
 def index():
@@ -29,17 +25,17 @@ def index():
 	return render_template("index.html")
 
 
-@app.route('/<username>')
+@app.route('/chat/<username>', methods = ["GET", "POST"])
 def user(username):
-	""" Display chat message """
-	return "<h1>Welcome, {0}</h1> {1}".format(username, get_all_messages())
-
-
-@app.route('/<username>/<message>')
-def send_message(username, message):
-	""" Creating a new message and redirect back to the chat page """
-	add_messages(username, message)
-	return redirect("/" + username)
+	""" add and display chat message """
+	
+	if request.method == "POST":
+		username = session["username"]
+		message = request.form["message"]
+		add_message(username, message)
+		return redirect(url_form("user", username=session["username"]))
+		
+	return render_template("chat.html", username = username, chat_messages = messages)
 
 
 
